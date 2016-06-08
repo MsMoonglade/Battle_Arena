091b8885010f928a17ccfeacabf5106dd09fa28b
Diego <diegogurrieri@live.com>
2016-06-08 17:26:33 +0200

    Selezione Personaggio
    Solo per un controller al momento

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -7,7 +7,9 @@ public class CharacterSelection : MonoBehaviour {
 	public GameObject Selector;
 	public UIButton[] ModelButton=new UIButton[4];
 	public int counter = 0;
-	public float SelectionTimer;
+	public float[] SelectionTimer = new float[4];
+
+	private float deadZone = 0.15f;
 
 	void Awake()
 	{ 
@@ -17,87 +19,71 @@ public class CharacterSelection : MonoBehaviour {
 	void Update(){
 
 		Debug.Log(GetAxis(0,"LeftRotationH"));
-		SelectionTimer += Time.deltaTime;
 
-		if (GetAxis (0, "LeftRotationH") >0 ) {
-			Debug.Log ("dio");
+
+		if (GetAxis (0, "LeftRotationH") < 0 ) {
+			SelectionTimer[0] += Time.deltaTime;
+			if (SelectionTimer[0] > deadZone) {
+				Debug.Log ("sinistra");
+				selectionLeft (0);
+			}
 		}
-			if (SelectionTimer > 0.5f) {
-			selectionRight ();
-			Debug.Log ("DESTRAAAA");
-		} 
-		else if 
-		(GetAxis (0, "RightRotationH") < 0) {
-
-			Debug.Log ("jesu");
-			if (SelectionTimer > 0.5f) {
-				selectionLeft ();
+			
+		else if (GetAxis (0, "LeftRotationH") > 0) {
+			SelectionTimer[0] += Time.deltaTime;
+			if (SelectionTimer[0] > deadZone) {
+				selectionRight (0); 
 			}
 		}
 
-
-
-
-
-		if (GetAxis (1, "LeftRotationH")>0) 
-		{
-			Debug.Log ("dio");
-			if(SelectionTimer>0.5f){
-				selectionLeft ();
-				Debug.Log ("?DESTRAAAA");
+		if (GetAxis (1, "LeftRotationH") < 0 ) {
+			SelectionTimer[1] += Time.deltaTime;
+			if (SelectionTimer[1] > deadZone) {
+				Debug.Log ("sinistra");
+				selectionLeft (1);
 			}
-		} else if (GetAxis (1, "LeftRotationH")<0)
-		if(SelectionTimer>0.5f){
-			selectionRight ();
 		}
-
-		if (GetAxis (2, "LeftRotationH") > 0) {
-			if (SelectionTimer > 0.5f) {
-				selectionLeft ();
-			}
-		} else if (GetAxis (2, "LeftRotationH") < 0)
-		if (SelectionTimer > 0.5f) {
-			selectionRight ();
-		}
-
-		if (GetAxis (3, "LeftRotationH")>0) 
-		{
-			if(SelectionTimer>0.5f){
-				selectionRight ();
-			}
-		} else if (GetAxis (3, "LeftRotationH")<0)
 		
-		if(SelectionTimer>0.5f){
-			selectionLeft ();
+		else if (GetAxis (1, "LeftRotationH") > 0) {
+			SelectionTimer[1] += Time.deltaTime;
+			if (SelectionTimer[1] > deadZone) {
+				selectionRight (1); 
+			}
 		}
+		
+
+
+
+
+
 
 	}
-	public void selectionRight(){
+	public void selectionRight(int playerId){
 
 
 		if (counter < 3) {
 			Selector.transform.position = ModelButton [counter + 1].transform.position;
 			counter++;
-			SelectionTimer=0;
+			SelectionTimer[playerId]=0;
 		
 		} else if (counter == 3) {
 			counter = 0; 
 			Selector.transform.position = ModelButton [counter].transform.position;
-			SelectionTimer=0;
+			SelectionTimer[playerId]=0;
 
 		}
 	}
-	public void selectionLeft(){
+	public void selectionLeft(int playerId){
 		if (counter != 0) {
 			Selector.transform.position = ModelButton [counter - 1].transform.position;
 			counter--;
-			SelectionTimer=0;
+			SelectionTimer[playerId]=0;
 
 		} else if (counter == 0) {
 			counter = 3;
 			Selector.transform.position = ModelButton [counter].transform.position;
 			Debug.Log ("workmnA");
-			SelectionTimer=0;
+			SelectionTimer[playerId]=0;
 
 		}
 	}

062c00800cf35b7781eef7d4a12d157bb7da2c28
Diego <diegogurrieri@live.com>
2016-06-08 17:44:21 +0200

    Selezione Personaggio e aggiunte sprite per la GUI
    2 Player muovono 2 cursori
    Ancora da implementare la selezione e il cambio immagini

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CharacterSelection : MonoBehaviour {
 
 
-	public GameObject Selector;
+	public GameObject[] CursorSelect=new GameObject[4];
 	public UIButton[] ModelButton=new UIButton[4];
 	public int counter = 0;
 	public float[] SelectionTimer = new float[4];
@@ -18,13 +18,9 @@ public class CharacterSelection : MonoBehaviour {
 
 	void Update(){
 
-		Debug.Log(GetAxis(0,"LeftRotationH"));
-
-
 		if (GetAxis (0, "LeftRotationH") < 0 ) {
 			SelectionTimer[0] += Time.deltaTime;
 			if (SelectionTimer[0] > deadZone) {
-				Debug.Log ("sinistra");
 				selectionLeft (0);
 			}
 		}
@@ -62,26 +58,26 @@ public class CharacterSelection : MonoBehaviour {
 
 
 		if (counter < 3) {
-			Selector.transform.position = ModelButton [counter + 1].transform.position;
+			CursorSelect[playerId].transform.position = ModelButton [counter + 1].transform.position;
 			counter++;
 			SelectionTimer[playerId]=0;
 		
 		} else if (counter == 3) {
 			counter = 0; 
-			Selector.transform.position = ModelButton [counter].transform.position;
+			CursorSelect[playerId].transform.position = ModelButton [counter].transform.position;
 			SelectionTimer[playerId]=0;
 
 		}
 	}
 	public void selectionLeft(int playerId){
 		if (counter != 0) {
-			Selector.transform.position = ModelButton [counter - 1].transform.position;
+			CursorSelect[playerId].transform.position = ModelButton [counter - 1].transform.position;
 			counter--;
 			SelectionTimer[playerId]=0;
 
 		} else if (counter == 0) {
 			counter = 3;
-			Selector.transform.position = ModelButton [counter].transform.position;
+			CursorSelect[playerId].transform.position = ModelButton [counter].transform.position;
 			Debug.Log ("workmnA");
 			SelectionTimer[playerId]=0;
 

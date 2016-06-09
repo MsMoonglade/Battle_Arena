a40294f468af5e342984499cbda1d12c012cc7a4
MsMoonglade <moonglade95@gmail.com>
2016-06-09 23:27:28 +0200

    inizio gui sui player

---
+++ Assets/Scripts/Player/PlayerGui.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGui : MonoBehaviour {
+
+    public UISlider HealthBar;
+    public UISlider EnergyBar;
+
+    private Player player;
+
+    void Awake()
+    {       
+        player = transform.parent.gameObject.GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.identity;
+
+        HealthBar.value = player.currentHealth / player.MaxHealth;
+        EnergyBar.value = player.currentEnergy / player.MaxEnergy;
+    }   
+}

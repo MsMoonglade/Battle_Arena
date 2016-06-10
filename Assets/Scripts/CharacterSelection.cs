82c446a4e79917419a8406a7e7638501dc8f1aff
Sbubino <ale.morello94@gmail.com>
2016-06-10 18:47:11 +0200

    Revert "Selezione Personaggi"
    This reverts commit 278d4cb13a401102e03d84da978da303a2c85899.

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -6,34 +6,17 @@ public class CharacterSelection : MonoBehaviour {
 
 	public GameObject[] CursorSelect=new GameObject[4];
 	public UIButton[] ModelButton=new UIButton[4];
-	public int[] counter = new int[4];    
+	public int counter = 0;
 	public float[] SelectionTimer = new float[4];
 
 	private float deadZone = 0.15f;
 
-
-
 	void Awake()
 	{ 
-
-		DontDestroyOnLoad (this);
+		 
 	}
 
 	void Update(){
-
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			Application.LoadLevel("Scena_Marco");
-		}
-
-
-//		if(GetButtonDown(0,"SelectA")){
-//			//			if(counter=0){
-//			//
-//			//			}
-//		}
-
-
-
 
 		if (GetAxis (0, "LeftRotationH") < 0 ) {
 			SelectionTimer[0] += Time.deltaTime;
@@ -41,7 +24,7 @@ public class CharacterSelection : MonoBehaviour {
 				selectionLeft (0);
 			}
 		}
-
+			
 		else if (GetAxis (0, "LeftRotationH") > 0) {
 			SelectionTimer[0] += Time.deltaTime;
 			if (SelectionTimer[0] > deadZone) {
@@ -56,14 +39,14 @@ public class CharacterSelection : MonoBehaviour {
 				selectionLeft (1);
 			}
 		}
-
+		
 		else if (GetAxis (1, "LeftRotationH") > 0) {
 			SelectionTimer[1] += Time.deltaTime;
 			if (SelectionTimer[1] > deadZone) {
 				selectionRight (1); 
 			}
 		}
-
+		
 
 
 
@@ -74,27 +57,27 @@ public class CharacterSelection : MonoBehaviour {
 	public void selectionRight(int playerId){
 
 
-		if (counter[playerId] < 3) {
-			CursorSelect[playerId].transform.position = ModelButton[counter[playerId] + 1].transform.position;
-			counter[playerId]++;
+		if (counter < 3) {
+			CursorSelect[playerId].transform.position = ModelButton [counter + 1].transform.position;
+			counter++;
 			SelectionTimer[playerId]=0;
-
-		} else if (counter[playerId] == 3) {
-			counter[playerId] = 0; 
-			CursorSelect[playerId].transform.position = ModelButton [counter[playerId]].transform.position;
+		
+		} else if (counter == 3) {
+			counter = 0; 
+			CursorSelect[playerId].transform.position = ModelButton [counter].transform.position;
 			SelectionTimer[playerId]=0;
 
 		}
 	}
 	public void selectionLeft(int playerId){
-		if (counter[playerId] != 0) {
-			CursorSelect[playerId].transform.position = ModelButton [counter[playerId] - 1].transform.position;
-			counter[playerId]--;
+		if (counter != 0) {
+			CursorSelect[playerId].transform.position = ModelButton [counter - 1].transform.position;
+			counter--;
 			SelectionTimer[playerId]=0;
 
-		} else if (counter[playerId] == 0) {
-			counter[playerId] = 3;
-			CursorSelect[playerId].transform.position = ModelButton [counter[playerId]].transform.position;
+		} else if (counter == 0) {
+			counter = 3;
+			CursorSelect[playerId].transform.position = ModelButton [counter].transform.position;
 			Debug.Log ("workmnA");
 			SelectionTimer[playerId]=0;
 
@@ -102,17 +85,17 @@ public class CharacterSelection : MonoBehaviour {
 	}
 
 
-	bool GetButton(int player, string name)
+	 bool GetButton(int player, string name)
 	{
 		return Rewired.ReInput.players.GetPlayer(player).GetButton(name);
 	}
-
-	bool GetButtonDown(int player, string name)
+	
+	 bool GetButtonDown(int player, string name)
 	{
 		return Rewired.ReInput.players.GetPlayer(player).GetButtonDown(name);
 	}
-
-	float GetAxis(int player, string name)
+	
+	 float GetAxis(int player, string name)
 	{
 		return Rewired.ReInput.players.GetPlayer(player).GetAxis(name);
 	}
--- Assets/Scripts/Player/LoadCharacter.cs
+++ Assets/Scripts/Player/LoadCharacter.cs
@@ -5,39 +5,16 @@ public class LoadCharacter : MonoBehaviour {
 
 
 	public GameObject[] Models;
-	//	public int Model;
-	public int IndexModel;
+	public int Model;
 
 	GameObject model;
 	private MeshRenderer[] mesh;
 
-	private GameObject ModelSelected;
-
 	void Awake()
 	{
-
-		if (this.gameObject.name == "Player1") {
-			IndexModel = 0;
-		}
-		if (this.gameObject.name == "Player2") {
-			IndexModel = 1;
-		}
-		if (this.gameObject.name == "Player3") {
-			IndexModel = 2;
-		}
-		if (this.gameObject.name == "Player4") {
-			IndexModel = 3;
-		}
-			
-		ModelSelected = GameObject.FindGameObjectWithTag ("CharacterController");
-
-		Debug.Log (ModelSelected.name);
-		Debug.Log (ModelSelected.GetComponent<CharacterSelection> ().counter [0]);
-
-		model = Instantiate (Models [ModelSelected.GetComponent<CharacterSelection>().counter[IndexModel]], transform.position, Quaternion.identity ) as GameObject;
+		model = Instantiate (Models [Model], transform.position, Quaternion.identity ) as GameObject;
 		model.transform.SetParent (transform);
 		model.name = "Model";
-
 
 		//mesh = model.transform.GetComponentsInChildren<MeshRenderer> ();
 		//model.transform.position =;

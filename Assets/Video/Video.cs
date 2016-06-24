4ddf6005513cb9434886da80af5ce115c8b8ed73
gullpacco <gullpacco@gmail.com>
2016-06-24 11:46:22 +0200

    Title Screen

---
+++ Assets/Video/Video.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Video : MonoBehaviour {
+
+
+
+    // Use this for initialization
+    public Material mat;
+    MovieTexture movie;
+    Renderer r;
+
+
+    void Awake() {
+        r = GetComponent<Renderer>();
+        ((MovieTexture)r.material.mainTexture).Play();
+         movie = (MovieTexture)r.material.mainTexture;
+        movie.loop = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            r.material = mat;
+            movie.Stop();
+            ((MovieTexture)r.material.mainTexture).Play();
+            movie = (MovieTexture)r.material.mainTexture;
+            movie.loop = false;
+            AudioManager.instance.PlaySound("M_Intro");
+        }
+	}
+
+ 
+}

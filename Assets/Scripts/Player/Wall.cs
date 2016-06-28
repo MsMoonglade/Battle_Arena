2c320ddfebebe2be5f224b17856fdb7b75e938ca
Sbubino <ale.morello94@gmail.com>
2016-06-28 18:24:49 +0200

    TI PREGO JESU

--- Assets/Scripts/Player/Wall.cs
+++ Assets/Scripts/Player/Wall.cs
@@ -4,15 +4,50 @@ using System.Collections;
 public class Wall : MonoBehaviour {
     public float ActiveTime = 3f;
 
+    
+
+    void Awake()
+    {
+        
+    }
+    
     void OnEnable()
     {
         StartCoroutine("Deactive");
+        StartCoroutine("Up");
+        StartCoroutine("Down");
+    }
+
+    void OnDisable()
+    {
+        transform.localScale = new Vector3(1, 0, 1);
     }
 
     IEnumerator Deactive()
     {
         yield return new WaitForSeconds(ActiveTime);
-        gameObject.SetActive(false);
+        gameObject.SetActive(false);       
+    }
+
+    IEnumerator Up()
+    {
+        while (transform.localScale.y < 1)
+        {
+           
+            transform.localScale = transform.localScale + new Vector3(0,1,0) * Time.deltaTime;
+            yield return null;
+        }
+        
+    }
+
+    IEnumerator Down()
+    {
+        yield return new WaitForSeconds(ActiveTime - 1);
+        while (transform.localScale.y > 0)
+        {
+            transform.localScale = transform.localScale - new Vector3(0, 1, 0) * Time.deltaTime;
+            yield return null;
+        }
     }
 	
 }

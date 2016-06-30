818cd291dfe52868c1dd2327b6d71cc8d0f657f9
Andrea <andrea.alicino@gmail.com>
2016-06-30 16:25:16 +0200

    Audio Title

--- Assets/Video/Video.cs
+++ Assets/Video/Video.cs
@@ -8,13 +8,24 @@ public class Video : MonoBehaviour {
     // Use this for initialization
   
 	private int firstTime;
+	public string shots= "M_Shots", intro ="M_Intro", mainTheme= "M_MainTheme";
 	Animator anim;
+	public int startTime=1;
+
 
     void Awake() {
 		firstTime = 0;
 		anim = GetComponent<Animator> ();
     }
 
+
+	void Start(){
+	
+		AudioManager.instance.PlaySound (intro);
+		AudioManager.instance.setVolume (intro, 0.1f);
+			AudioManager.instance.FadeMusic(intro,0.7f,0.5f);
+
+	}
     // Update is called once per frame
     void Update () {
 
@@ -22,10 +33,17 @@ public class Video : MonoBehaviour {
 		if (Input.anyKeyDown && firstTime <= 0)
         {
 			anim.SetBool("isStarted",true);
-            AudioManager.instance.PlaySound("M_Intro");
+            AudioManager.instance.PlaySound(shots);
+			AudioManager.instance.FadeMusic(intro,0,0.5f);
 			firstTime++;
+			Invoke("StartMainTheme", startTime);
         }
 	}
 
+
+	void StartMainTheme(){
+
+		AudioManager.instance.PlaySound (mainTheme);
+	}
  
 }
